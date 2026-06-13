46a9b2dbc88dd3e7a8cfc1dfa950c8c813985df4
Padddie-arch <akanbidaniel99@gmail.com>
2026-06-13 16:12:26 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,7 @@ const tradeHistory = require('./models/tradeHistory');
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static('webapp'));
 const server = http.createServer(app);
 const io = socketIo(server, { cors: { origin: "*" } });
 
